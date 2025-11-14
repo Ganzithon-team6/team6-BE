42a2583c74cd236515c1a4c381007dc2e4d145d6
Wish <asgvcx@naver.com>
2025-11-14 16:09:23 +0900

    chore: 기초 세팅 완료

---
+++ ganjiton/src/main/java/com/ganjiton/ganjiton/GanjitonApplication.java
@@ -0,0 +1,13 @@
+package com.ganjiton.ganjiton;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GanjitonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GanjitonApplication.class, args);
+	}
+
+}
---
+++ ganjiton/src/test/java/com/ganjiton/ganjiton/GanjitonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ganjiton.ganjiton;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GanjitonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
