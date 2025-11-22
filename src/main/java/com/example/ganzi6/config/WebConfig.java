2d431c9756d8fdc9b3bb37c7d4ee841724265153
Wish <asgvcx@naver.com>
2025-11-22 13:17:51 +0900

    feat: 프론트 배포 준비 완료

--- src/main/java/com/example/ganzi6/config/WebConfig.java
+++ src/main/java/com/example/ganzi6/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins(
-                        //"배포프론트꺼나중에추가",
+                        "https://fooda-theta.vercel.app",
                         "http://localhost:5173"
                 )
                 .allowedMethods("*")
